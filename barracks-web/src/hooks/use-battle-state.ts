4095336a20f6091e86186b3ead25ff5cfdf9db1d
Francois Laubscher <francois.laubscher@toptal.com>
2023-08-29 20:54:11 +0200

    feature: render dice bag

--- barracks-web/src/hooks/use-battle-state.ts
+++ barracks-web/src/hooks/use-battle-state.ts
@@ -1,13 +1,13 @@
-import { useCallback, useReducer } from 'react';
+import { useCallback, useMemo, useReducer } from 'react';
 
 type BattleState = {
   round: number;
   diceBag: string[];
-  active: 'PLAYER' | 'AI' | undefined;
+  actor: 'PLAYER' | 'RAX' | 'NONE';
 };
 
 type BattleAction = {
-  type: 'INITIALIZE' | 'DRAW_ORDER_DIE' | 'COMPLETE_ROUND' | 'COMPLETE_BATTLE';
+  type: 'INITIALIZE' | 'DRAW_ORDER_DIE' | 'COMPLETE_ROUND';
   payload?: CompleteRoundPayload;
 };
 
@@ -18,33 +18,46 @@ type CompleteRoundPayload = {
 
 type BattleStateReducer = (state: BattleState, action: BattleAction) => BattleState;
 
+const INITIAL_STATE: BattleState = {
+  actor: 'NONE',
+  diceBag: [],
+  round: 0
+};
+
 const reducer: BattleStateReducer = (state, action) => {
+  console.log(`🪖 RAX 🪖: ${action.type}`, action.payload);
+
   switch (action.type) {
+    case 'INITIALIZE':
+      return {
+        actor: 'NONE',
+        diceBag: fillDiceBag(action.payload?.totalPlayerDice, action.payload?.totalAiDice),
+        round: 1
+      };
     case 'DRAW_ORDER_DIE':
       const newDiceBag = [...state.diceBag];
       const index = Math.floor(Math.random() * newDiceBag.length);
-      const active = newDiceBag[index] as 'PLAYER' | 'AI';
+      const active = newDiceBag[index] as BattleState['actor'];
       newDiceBag.splice(index, 1);
 
-      return { ...state, active, diceBag: newDiceBag };
-    case 'INITIALIZE':
+      return { ...state, actor: active, diceBag: newDiceBag };
     case 'COMPLETE_ROUND':
-      const { totalPlayerDice, totalAiDice } = action.payload as CompleteRoundPayload;
+      const payload = action.payload as CompleteRoundPayload;
 
       return {
         ...state,
-        active: undefined,
-        diceBag: fillDiceBag(totalPlayerDice, totalAiDice),
+        actor: 'NONE',
+        diceBag: fillDiceBag(action.payload?.totalPlayerDice, action.payload?.totalAiDice),
         round: state.round + 1
       };
-    case 'COMPLETE_BATTLE':
-      return { ...state };
     default:
       return state;
   }
 };
 
-const fillDiceBag = (totalPlayerDice: number, totalAiDice: number) => {
+const fillDiceBag = (totalPlayerDice: number = 0, totalAiDice: number = 0) => {
+  if (totalPlayerDice === 0 && totalAiDice === 0) return [];
+
   const diceBag: string[] = [];
 
   for (let i = 0; i < totalPlayerDice; i++) {
@@ -52,7 +65,7 @@ const fillDiceBag = (totalPlayerDice: number, totalAiDice: number) => {
   }
 
   for (let i = 0; i < totalAiDice; i++) {
-    diceBag.push('AI');
+    diceBag.push('RAX');
   }
 
   return diceBag
@@ -61,16 +74,23 @@ const fillDiceBag = (totalPlayerDice: number, totalAiDice: number) => {
     .map(({ value }) => value);
 };
 
+const getNextAction = (state: BattleState): BattleAction['type'] => {
+  if (state.round === 0) {
+    return 'INITIALIZE';
+  }
+
+  if (state.diceBag.length === 0) {
+    return 'COMPLETE_ROUND';
+  }
+
+  return 'DRAW_ORDER_DIE';
+};
+
 const useBattleState = () => {
-  const [state, dispatch] = useReducer(reducer, {
-    round: 0,
-    diceBag: [],
-    active: undefined
-  });
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
   const handleInitialize = useCallback(
     (totalPlayerDice: number, totalAiDice: number) => {
-      console.log('initializing');
       dispatch({ type: 'INITIALIZE', payload: { totalPlayerDice, totalAiDice } });
     },
     [dispatch]
@@ -87,9 +107,22 @@ const useBattleState = () => {
     [dispatch]
   );
 
+  const memoedState = useMemo(() => {
+    return {
+      actor: state.actor,
+      round: state.round,
+      diceBag: {
+        dice: state.diceBag,
+        player: state.diceBag.filter((d) => d === 'PLAYER').length,
+        rax: state.diceBag.filter((d) => d === 'RAX').length
+      },
+      nextAction: getNextAction(state)
+    };
+  }, [state]);
+
   return {
-    state,
-    drawDice: handleDrawDice,
+    state: memoedState,
+    drawOrderDie: handleDrawDice,
     completeRound: handleCompleteRound,
     initialize: handleInitialize
   };
--- barracks-web/src/pages/battle/ai.tsx
+++ barracks-web/src/pages/battle/ai.tsx
@@ -1,7 +1,10 @@
+import { useMemo } from 'react';
+import classnames from 'classnames';
 import { Button, Stack, Stat } from '@fjlaubscher/matter';
 import { useParams } from 'react-router-dom';
 
 // components
+import CompleteRoundForm from '../../components/battle/complete-round-form';
 import Layout from '../../components/layout';
 
 // helpers
@@ -11,7 +14,8 @@ import { calculateOrderDice } from '../../helpers/list';
 import { useBattle } from '../../hooks/battle';
 import useBattleState from '../../hooks/use-battle-state';
 import { useList } from '../../hooks/list';
-import { useMemo } from 'react';
+
+import styles from './battle.module.scss';
 
 const AIBattle = () => {
   const { key } = useParams();
@@ -24,7 +28,7 @@ const AIBattle = () => {
 
   const playerDice = useMemo(() => calculateOrderDice(playerList), [playerList]);
   const aiDice = useMemo(() => calculateOrderDice(aiList), [aiList]);
-  const { state, drawDice, initialize } = useBattleState();
+  const { state, initialize, drawOrderDie, completeRound } = useBattleState();
 
   const loading = loadingBattle || loadingPlayerList || loadingAiList;
 
@@ -35,11 +39,40 @@ const AIBattle = () => {
       isLoading={loading}
     >
       <Stack direction="column">
-        <Stat title={`Round ${state.round}`} value={state.active || 'NONE'} />
-        {state.diceBag.length ? (
-          <Button onClick={() => drawDice()}>Draw Dice</Button>
-        ) : (
-          <Button onClick={() => initialize(playerDice, aiDice)}>End Round</Button>
+        <Stat title={`Round ${state.round}`} value={state.actor} />
+        {state.nextAction === 'INITIALIZE' && (
+          <Button variant="success" onClick={() => initialize(playerDice, aiDice)}>
+            Start
+          </Button>
+        )}
+        {state.nextAction === 'DRAW_ORDER_DIE' && (
+          <>
+            <div
+              className={styles.diceBag}
+              style={{ gridTemplateColumns: `repeat(${state.diceBag.dice.length}, 4rem)` }}
+            >
+              {state.diceBag.dice.map((d, i) => (
+                <div
+                  key={`order-die-${i}`}
+                  className={classnames(
+                    styles.orderDie,
+                    i === 0 ? styles[state.actor.toLowerCase()] : undefined
+                  )}
+                >
+                  {i === 0 && state.actor !== 'NONE' ? state.actor : ''}
+                </div>
+              ))}
+            </div>
+            <Button variant="info" onClick={() => drawOrderDie()}>
+              Draw Order Die
+            </Button>
+          </>
+        )}
+        {state.nextAction === 'COMPLETE_ROUND' && (
+          <CompleteRoundForm
+            initialValues={{ totalAiDice: aiDice, totalPlayerDice: playerDice }}
+            onSubmit={(values) => completeRound(values.totalPlayerDice, values.totalAiDice)}
+          />
         )}
       </Stack>
     </Layout>
